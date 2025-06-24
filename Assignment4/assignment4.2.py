acf5c409b9d12c202154766e11ee148eb7693a37
Parveen Kumar <parveenyadavpy57377@gmail.com>
2025-06-25 03:03:10 +0530

    uploded

--- Assignment4/assignment4.2.py
+++ Assignment4/assignment4.2.py
@@ -1,3 +1,5 @@
+import requests
+
 def weather_details(city):
     url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2e2cac1a891061ca51e204da2f9fc18b"
     final_url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2e2cac1a891061ca51e204da2f9fc18b&units=metric"
@@ -8,11 +10,37 @@ def weather_details(city):
 #format of data json
         temp=data1["main"]["temp"]
         humidity=data1["main"]["humidity"]
+        country=data1["sys"]["country"]
         print(f"{temp}{chr(176)}")
         print(humidity)
+        print(country)
 
 
     except requests.exceptions.RequestException as e:
         print(f" error fetch weather data:{e}")
 city=input("enter the  city")
 weather_details(city)
+
+
+# {"coord":{"lon":75.8167,"lat":26.9167},
+#  "weather":[{"id":804,"main":"Clouds","description":"overcast clouds","icon":"04d"}],
+#  "base":"stations",
+#  "main":{"temp":302.35,"feels_like":306.52,"temp_min":302.35,"temp_max":302.35,"pressure":1000,"humidity":71,"sea_level":1000,"grnd_level":954},
+#  "visibility":10000,
+#  "wind":{"speed":7.96,"deg":260,"gust":8.37},
+#  "clouds":{"all":100},"dt":1750652531,
+#  "sys":{"country":"IN","sunrise":1750637032,"sunset":1750686832},
+#  "timezone":19800,
+#  "id":1269515,
+#  "name":"Jaipur",
+#  "cod":200}
+# temprature=data["coord"]["lon"]
+# print(temprature)
+
+#{"coord":{"lon":75.8167,"lat":26.9167},
+# "weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],
+# "base":"stations",
+# "main":{"temp":29.18,"feels_like":36.18,"temp_min":29.18,"temp_max":29.18,"pressure":998,"humidity":89,"sea_level":998,"grnd_level":950},
+# "visibility":10000,
+# "wind":{"speed":1.13,"deg":213,"gust":1.53},
+# "clouds":{"all":61},"dt":1750800216,"sys":{"country":"IN","sunrise":1750809861,"sunset":1750859651},"timezone":19800,"id":1269515,"name":"Jaipur","cod":200}
--- Assignment4/assignment4.3.py
+++ Assignment4/assignment4.3.py
@@ -1,5 +1,6 @@
 import sqlite3
 conn=sqlite3.connect("db2.db")
+conn.execute("DROP TABLE IF EXISTS student1")
 conn.execute('''
 CREATE TABLE student1(
 stid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -7,6 +8,7 @@ stname varchar(255) NOT NULL,
 branch varchar(255),
 year int)
 ''')
+conn.execute("DROP TABLE IF EXISTS employe1")
 conn.execute('''
 CREATE TABLE employe1(
 emid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,6 +16,7 @@ emname varchar(255),
 Addr varchar(255)
 )
 ''')
+conn.execute("DROP TABLE IF EXISTS product1")
 conn.execute('''
 CREATE TABLE product1(
 prid INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,24 +24,50 @@ prname varchar(255),
 prQuantity INT NOT NULL
 )
 ''')
+print("table created succesfully")
+
+conn.execute('''
+INSERT INTO student1(stname,branch,year)VALUES
+('parveen','AI',2),
+('riya','AI',2),
+('yadav','CS',3)
+''')
+conn.execute('''
+INSERT INTO employe1(emname,Addr) VALUES
+('parveen','JAIPUR'),
+('riya','HANUMANGARH'),
+('yadav','rewari')
+
+''')
+conn.commit()
+
+conn.execute('''
+INSERT INTO product1(prname,prQuantity) VALUES
+('Soap',10),
+('butter',100),
+('milk',20)
+
+''')
+conn.commit()
+
+
+data=conn.execute("Select * from student1")
+for x in  data:
+    print(x)
+
+data=conn.execute("Select * from employe1")
+for x in  data:
+    print(x)
+
+#for update the values
+
+conn.execute("UPDATE student1 SET stname='hemang' where stid=1")
+conn.commit()
+
+#for deleting anything from database
+
+conn.execute("DELETE from student1 where stid=2")
+conn.commit()
 conn.close()
-conn.execute('''
-INTEGER INTO student1(stname,branch,year) VALUES(
-('parveen','AI',2),('riya','AI',2),('yadav','CS','3'))
 
-''')
-conn.commit()
-conn.execute('''
-INTEGER INTO employe1(emname,Addr) VALUES(
-('parveen','JAIPUR'),('riya','HANUMANGARH'),('yadav','rewari'))
-
-''')
-conn.commit()
-conn.execute('''
-INTEGER INTO student1(prname,prQuantity) VALUES(
-('Soap','10'),('butter','100'),('milk','20'))
-
-''')
-conn.commit()
-conn.close()
 
